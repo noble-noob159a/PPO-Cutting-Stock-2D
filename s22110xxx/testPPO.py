44592728f67511dfc4f5fa488309fe4c09cd3034
Luong Minh Thuan <116414953+noble-noob159a@users.noreply.github.com>
2024-11-24 22:20:33 +0700

    Update testPPO.py

--- s22110xxx/testPPO.py
+++ s22110xxx/testPPO.py
@@ -1,29 +1,21 @@
 import gymnasium as gym
-import sys
-import torch
-from Policy2210xxx import PPO
+from Policy2210xxx import *
 import gym_cutting_stock
-
-
-def train(env, actor_model, critic_model):
-    print(f"Training", flush=True)
-
-    model = PPO(env=env)
-    if actor_model != '' and critic_model != '':
-        print(f"Loading in {actor_model} and {critic_model}...", flush=True)
-        model.actor.load_state_dict(torch.load(actor_model,weights_only=True))
-        model.critic.load_state_dict(torch.load(critic_model,weights_only=True))
-        print(f"Successfully loaded.", flush=True)
-    else:
-        print(f"Training from scratch.", flush=True)
-    model.learn(total_timesteps=200_000_000)
-
 
 env = gym.make(
     "gym_cutting_stock/CuttingStock-v0",
-    #render_mode="human",  # Comment this line to disable rendering
+    render_mode="human",  # Comment this line to disable rendering
 )
 
-actorDir = './models/ppo_actor.pth'
-criticDir ='./models/ppo_critic.pth'
-train(env, actorDir, criticDir)
+agent = PPO()
+observation, info = env.reset(seed=42)
+#agent.train(total_timestep=50_000)
+ep = 0
+while ep < 1:
+    action = agent.get_action(observation, info)
+    observation, reward, terminated, truncated, info = env.step(action)
+    # print(observation["stocks"])
+    if terminated or truncated:
+        print(info)
+        observation, info = env.reset(seed=ep)
+        ep += 1
