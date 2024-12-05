1e7f2f99d81d650338dc45aecdace262c0a9b596
Luong Minh Thuan <116414953+noble-noob159a@users.noreply.github.com>
2024-12-05 18:56:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import gym_cutting_stock
 import gymnasium as gym
 from policy import GreedyPolicy, RandomPolicy
-from student_submissions.s2210xxx.policy2210xxx import Policy2210xxx
+from s2210xxx.policy2210xxx import Policy2210xxx
 
 # Create the environment
 env = gym.make(
