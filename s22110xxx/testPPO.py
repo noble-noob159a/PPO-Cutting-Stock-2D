1ae9e8cc805849b4b64e2e029c52effdabf6aa25
Luong Minh Thuan <116414953+noble-noob159a@users.noreply.github.com>
2024-12-05 17:51:57 +0700

    Update testPPO.py

--- s22110xxx/testPPO.py
+++ s22110xxx/testPPO.py
@@ -2,20 +2,28 @@ import gymnasium as gym
 from Policy2210xxx import *
 import gym_cutting_stock
 
+
+test_mode = True
+load_check_point = True
+
+
+render = "human" if test_mode else None
 env = gym.make(
     "gym_cutting_stock/CuttingStock-v0",
-    render_mode="human",  # Comment this line to disable rendering
+    render_mode=render,  # Comment this line to disable rendering
 )
-
-agent = PPO()
-observation, info = env.reset(seed=42)
-#agent.train(total_timestep=50_000)
-ep = 0
-while ep < 1:
-    action = agent.get_action(observation, info)
-    observation, reward, terminated, truncated, info = env.step(action)
-    # print(observation["stocks"])
-    if terminated or truncated:
-        print(info)
-        observation, info = env.reset(seed=ep)
-        ep += 1
+train_env = None if test_mode else env
+agent = PPO(train_env,load_check_point)
+if not test_mode:
+    agent.train(total_timestep=100_000)
+else:
+    observation, info = env.reset(seed=42)
+    ep = 0
+    while ep < 10:
+        action = agent.get_action(observation, info)
+        observation, reward, terminated, truncated, info = env.step(action)
+        # print(observation["stocks"])
+        if terminated or truncated:
+            print(info)
+            observation, info = env.reset(seed=ep)
+            ep += 1
