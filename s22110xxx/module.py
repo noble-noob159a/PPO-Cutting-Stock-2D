c6d50e84a74a763a1c48f87c5a3f06e3e1d39e52
Luong Minh Thuan <116414953+noble-noob159a@users.noreply.github.com>
2024-12-05 17:51:44 +0700

    Update module.py

--- s22110xxx/module.py
+++ s22110xxx/module.py
@@ -3,129 +3,55 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.distributions import Normal
+from torch.distributions import Categorical
 
 
 class FilterCNN(nn.Module):
-    def __init__(self, out_dim=256):
+    def __init__(self, out_dim=64):
         super(FilterCNN, self).__init__()
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-
-        # Global average pooling to handle dynamic input sizes
-        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
-
-        # Fully connected layers for fixed-size output
-        self.fc_layers = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(128, out_dim),
-            nn.ReLU(),
-        )
+        self.conv1 = nn.Conv2d(3, out_dim, kernel_size=1)
+        self.bn1 = nn.LazyBatchNorm2d()
+        self.conv2 = nn.Conv2d(out_dim, 1, kernel_size=1)
+        self.bn2 = nn.LazyBatchNorm2d()
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-    def forward(self, x):
-        x = self.conv_layers(x)  # Pass through convolutional layers
-        x = self.global_pool(x)  # Apply global average pooling
-        x = self.fc_layers(x)  # Fully connected layer for embedding
+    def forward(self, x, mask, bias_mask=None):
+        x = torch.cat([x.unsqueeze(dim=1), mask.unsqueeze(dim=1), bias_mask.unsqueeze(dim=1)], dim=1)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
         return x
 
 
-class ActorNetwork(nn.Module):
+class ActorCritic(nn.Module):
     def __init__(self, n_actions, filter_out, alpha, chkpt_dir='./models'):
-        super(ActorNetwork, self).__init__()
+        super(ActorCritic, self).__init__()
 
-        self.checkpoint_file = os.path.join(chkpt_dir, 'actor_torch_ppo')
-        self.actor = nn.Sequential(
-            nn.Linear(filter_out*3 + 2, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, n_actions),
-        )
+        self.checkpoint_file = os.path.join(chkpt_dir, 'ac_torch_ppo1')
+        self.actor = nn.Conv2d(1, 1, kernel_size=1)
         self.critic = nn.Sequential(
-            nn.Linear(filter_out*3 + 2, 256),
+            nn.LazyLinear(out_features=128),
             nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 1),
+            nn.LazyLinear(out_features=1),
         )
         self.filter = FilterCNN(filter_out)
-
-        self.log_std = nn.Parameter(torch.zeros(n_actions, requires_grad=True, dtype=torch.float32), requires_grad=True)
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        self.to(self.device)
-
-    def extractState(self,state):
-        stock1 = []
-        stock2 = []
-        stock3 = []
-        pro_size = []
-        for s in state:
-            stock1.append(torch.tensor(s[0],dtype=torch.float).unsqueeze(dim=0).unsqueeze(dim=0).to(self.device))
-            stock2.append(torch.tensor(s[1],dtype=torch.float).unsqueeze(dim=0).unsqueeze(dim=0).to(self.device))
-            stock3.append(torch.tensor(s[2],dtype=torch.float).unsqueeze(dim=0).unsqueeze(dim=0).to(self.device))
-            pro_size.append(torch.tensor(s[3],dtype=torch.float).to(self.device))
-            #print(stock1[0].shape)
-        return [stock1,stock2,stock3],pro_size
-
-    def forward(self, state):
-        stocks, pro_size = self.extractState(state)
-        state = torch.tensor([],dtype=torch.float).to(self.device)
-        for i in range(len(pro_size)):
-            s1,s2,s3 = stocks[0][i],stocks[1][i],stocks[2][i]
-            f1,f2,f3 = self.filter(s1).squeeze(dim=0),self.filter(s2).squeeze(dim=0),self.filter(s3).squeeze(dim=0)
-            #print(f1.shape,f2.shape,f3.shape,pro_size[i].shape)
-            batch = torch.cat([f1,f2,f3,pro_size[i]],dim=0)
-            state = torch.cat([state,batch.unsqueeze(dim=0)],dim=0)
-        #print(state.shape)
-        dist = self.actor(state)
-        sigma = torch.exp(self.log_std)
-        dist = Normal(dist, sigma)
-        return dist, self.critic(state)
-
-    def save_checkpoint(self):
-        torch.save(self.state_dict(), self.checkpoint_file)
-
-    def load_checkpoint(self):
-        self.load_state_dict(torch.load(self.checkpoint_file))
-        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        self.to(self.device)
-
-
-class CriticNetwork(nn.Module):
-    def __init__(self, input_dims, alpha, chkpt_dir='./models'):
-        super(CriticNetwork, self).__init__()
-
-        self.checkpoint_file = os.path.join(chkpt_dir, 'critic_torch_ppo')
-        self.critic = nn.Sequential(
-            nn.Linear(input_dims, 2048),
-            nn.ReLU(),
-            nn.Linear(2048, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Linear(512, 1),
-        )
         self.optimizer = optim.Adam(self.parameters(), lr=alpha)
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.to(self.device)
 
     def forward(self, state):
-        value = self.critic(state)
-        return value
+        stock, bias_mask, mask = state[:, 0], state[:, 1], state[:, 2]
+        f = self.filter(stock, mask, bias_mask)
+        x = self.actor(f)
+        x = x.masked_fill(mask == 0, -1e9)
+        x = torch.flatten(x, start_dim=-3)
+        x = torch.softmax(x, dim=-1)
+        dist = Categorical(x)
+        f = torch.flatten(f, start_dim=-3)
+        value = self.critic(f)
+        return dist, value
 
     def save_checkpoint(self):
         torch.save(self.state_dict(), self.checkpoint_file)
@@ -154,7 +80,7 @@ class PPOMemory:
         np.random.shuffle(indices)
         batches = [indices[i:i + self.batch_size] for i in batch_start]
 
-        return np.array(self.states,dtype=object), \
+        return np.array(self.states), \
             np.array(self.actions), \
             np.array(self.probs), \
             np.array(self.vals), \
@@ -164,7 +90,7 @@ class PPOMemory:
 
     def store_memory(self, state, action, probs, vals, reward, done):
         self.states.append(state)
-        self.actions.append(action.cpu())
+        self.actions.append(action)
         self.probs.append(probs)
         self.vals.append(vals)
         self.rewards.append(reward)
