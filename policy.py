8a8db58d7af027561bae000b295669a8d8f55712
Luong Minh Thuan <116414953+noble-noob159a@users.noreply.github.com>
2024-12-10 09:40:45 +0700

    Update policy.py

--- policy.py
+++ policy.py
@@ -90,22 +90,32 @@ class GreedyPolicy(Policy):
                 for i, stock in enumerate(observation["stocks"]):
                     stock_w, stock_h = self._get_stock_size_(stock)
                     prod_w, prod_h = prod_size
-
-                    if stock_w < prod_w or stock_h < prod_h:
-                        continue
-
-                    pos_x, pos_y = None, None
-                    for x in range(stock_w - prod_w + 1):
-                        for y in range(stock_h - prod_h + 1):
-                            if self._can_place_(stock, (x, y), prod_size):
-                                pos_x, pos_y = x, y
+                    if stock_w >= prod_w and stock_h >= prod_h:
+                        pos_x, pos_y = None, None
+                        for x in range(stock_w - prod_w + 1):
+                            for y in range(stock_h - prod_h + 1):
+                                if self._can_place_(stock, (x, y), prod_size):
+                                    pos_x, pos_y = x, y
+                                    break
+                            if pos_x is not None and pos_y is not None:
                                 break
                         if pos_x is not None and pos_y is not None:
+                            stock_idx = i
                             break
 
-                    if pos_x is not None and pos_y is not None:
-                        stock_idx = i
-                        break
+                    if stock_w >= prod_h and stock_h >= prod_w:
+                        pos_x, pos_y = None, None
+                        for x in range(stock_w - prod_h + 1):
+                            for y in range(stock_h - prod_w + 1):
+                                if self._can_place_(stock, (x, y), prod_size[::-1]):
+                                    prod_size = prod_size[::-1]
+                                    pos_x, pos_y = x, y
+                                    break
+                            if pos_x is not None and pos_y is not None:
+                                break
+                        if pos_x is not None and pos_y is not None:
+                            stock_idx = i
+                            break
 
                 if pos_x is not None and pos_y is not None:
                     break
