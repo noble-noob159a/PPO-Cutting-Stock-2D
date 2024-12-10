df0311a7c0cf026d3ebff7131e5646c892943b28
Luong Minh Thuan <116414953+noble-noob159a@users.noreply.github.com>
2024-12-10 09:43:43 +0700

    Update testPPO.py

--- s22110xxx/testPPO.py
+++ s22110xxx/testPPO.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
-from s22110xxx.policy2210xxx import Policy2210xxx
+from heuristic_policy import CPolicy
+from ppo_policy import PPO
 import gym_cutting_stock
 
 
@@ -13,10 +14,22 @@ env = gym.make(
     render_mode=render,  # Comment this line to disable rendering
 )
 train_env = None if test_mode else env
-agent = Policy2210xxx(train_env,load_check_point)
+agent = PPO(train_env,load_check_point)
 if not test_mode:
     agent.train(total_timestep=100_000)
 else:
+    observation, info = env.reset(seed=42)
+    ep = 35
+    while ep < 30:
+        action = agent.get_action(observation, info)
+        observation, reward, terminated, truncated, info = env.step(action)
+        # print(observation["stocks"])
+        if terminated or truncated:
+            print(info)
+            observation, info = env.reset(seed=ep)
+            ep += 1
+    print("agent2 result: ")
+    agent = CPolicy()
     observation, info = env.reset(seed=42)
     ep = 0
     while ep < 10:
